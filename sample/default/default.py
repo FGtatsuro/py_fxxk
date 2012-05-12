835102e8f0a4446116a996243f96e386409836b2
FGtatsuro <fujiistorage@gmail.com>
2012-05-13 03:28:46 +0900

    add sample for default operators.

---
+++ sample/default/default.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from py_fxxk import BrainFxxk
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit(1)
+    BrainFxxk().fxxk(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
